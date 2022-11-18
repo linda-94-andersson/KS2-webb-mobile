5027f626d1113cd3f1175b950c7428e0b40d7944
Linda Andersson <linda.29.andersson@gmail.com>
2022-11-18 12:23:44 +0100

    Updated TS types and fucntion to stop undefined

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -15,28 +15,36 @@ type Props = {
 type User = {
   id: string;
   name: string;
+  map: Function;
 };
 
 type Value = {
   userValue: {
-    users: User | null;
+    users: User | undefined;
+    setUser: React.Dispatch<React.SetStateAction<User | undefined>>;
   };
-  getUserData: () => void;
+  getUserData: () => Promise<void>;
 };
 
 const UserContext = createContext<Value | undefined>(undefined);
 
-export function useUser() {
-  return useContext(UserContext);
-}
+export const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error("useAuthContext must be used inside AuthContext");
+  }
+
+  return context;
+};
 
 export function UserProvider({ children }: Props) {
-  const [users, setUser] = useState(null);
+  const [users, setUser] = useState<User | undefined>();
 
   const userValue = useMemo(() => ({ users, setUser }), [users, setUser]);
 
   const getUserData = async () => {
-    const data = await getUsers();
+    const data: User = await getUsers();
     setUser(data);
   };
 
